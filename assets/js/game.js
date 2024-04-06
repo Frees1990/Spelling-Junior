497751ec3d821d1f0b7a1259a7d43ffe91e99cc5
Frees1990 <fairmejor@gmail.com>
2024-04-06 13:36:13 +0000

    add code to increment correct and incorrect answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,24 @@
 let word;
     // words to use for the spelling
-const words = ["accident", "accurate", "actual", "adult", "address", "afraid", "answer", "appear", "April", "arrive", "attention", "August", "Autumn", "believe", "bicycle", "Birthday", "breath", "build", "busy", "business", "calculate", "calendar", "cartoon", "caught", "centre","century","certain", "chocolate", "Christmas", "circle","complete", "communicacte", "consider","continue", "corpse", "computer", "couch", "current", "decide","December", "debt","delicious", "describe","different","difficult","disappear", "dessert", "dream", "early","earth","eight","enough", "exercise", "excited", "experience","experiment","extreme","famous","favourite","February", "figure", "forward","Friday","fruit", "general", "generate", "grammar","group","guard","guide", "Halloween", "heard","heart","height","history", "horrizon", "hour","imagine", "increase", "important", "instinct", "interest", "island","January", "judge", "June", "July", "knowledge", "learn", "length", "library", "light", "magic", "March", "May", "material", "medicine", "mention", "minute", "mobile", "Monday", "movie", "multiply", "natural", "narrate", "naughty", "notice", "November", "occasion", "October", "often", "opposite", "ordinary", "particular", "peculiar", "perhaps", "popular", "position", "possession", "possible", "potatoes", "pressure", "probably", "promise", "purpose", "quarter", "question", "radio", "random", "recent", "regular", "reign", "remember", "respect", "Saturday", "scent", "scene", "science", "seconds", "September", "sense", "sentence", "Spring", "separate", "simple","special", "shallow", "straight", "strange", "strength", "Summer", "Sunday", "suppose", "surprise", "square", "tangle", "telephone", "television", "terrified", "therefore","though", "thought", "Thursday", "through", "triangle", "Tuesday", "Universe", "unite", "Valentines", "various", "weight", "Wednesday", "Winter", "woman", "xylophone" ]
+const words = ["accident", "accurate", "actual", "accept","accurate", "adult", "address", "afraid", "answer", "appear", "April", "arrive", "attention", "August", "Autumn", "believe", "bicycle", "Birthday", "breath", "build", "busy", "business", "calculate", "calendar", "cartoon", "caught", "category", "catalog", "centre","century","certain", "chocolate", "Christmas", "circle","complete", "communicacte", "consider","continue", "corpse", "computer", "credit", "couch", "current", "decide","December", "debt", "debit","delicious", "describe","different","difficult","disappear", "dessert", "dream", "early","earth","eight","enough", "exercise", "excited", "exception", "experience","experiment","extreme","famous","favourite","February", "figure", "forward","Friday","fruit", "general", "generate", "grammar","group","guard","guide", "Halloween", "heard","heart","height","history", "horrizon", "hour","imagine", "image", "increase", "important", "instinct", "interest", "island","January", "judge", "June", "July", "knowledge", "learn", "length", "library", "light", "magic", "March", "May", "material", "medicine", "mention", "minute", "mobile", "Monday", "movie", "multiply", "natural", "narrate", "naughty", "notice", "November", "occasion", "october", "often", "opposite", "omnivor", "ordinary", "particular", "peculiar", "perhaps", "popular", "position", "possession", "possible", "potatoes", "pressure", "probably", "promise", "purpose", "quarter", "question", "radio", "random", "recent", "regular", "reign", "remember", "respect", "Saturday", "scent", "scene", "science", "seconds", "September", "sense", "sentence", "Spring", "separate", "simple","special", "shallow", "straight", "strange", "strength", "Summer", "Sunday", "suppose", "surprise", "square", "tangle", "telephone", "television", "terrified", "therefore","though", "thought", "Thursday", "through", "triangle", "Tuesday", "Universe", "unite", "Valentines", "various", "weight", "Wednesday", "Winter", "woman", "xylophone"]
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("id") === "check") {
+                check();
+            } 
+            else {
+                let spellingGame = this.getAttribute("id");
+                generate("id");
+            }
+        });
+    }
+});
+
 
 // This here is to generate the words above on shuffle"random"
 
@@ -13,30 +31,46 @@ document.getElementById("generate").addEventListener("click", function(){
         speechSynthesis.speak(utterance)
 });
 
+// This for the repeat button, provided for user to repeat the word in case they missed it the first time, copied and pasted another one to repeat again. 
+
+document.getElementById("talk").addEventListener("click", function(){
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text=word
+    speechSynthesis.speak(utterance)
+});
+
+
 // This here is for the check button to check if the word entered was correct or not, there is alsoo an alert to let the user know if the word enetered is correct. 
+
+
+
 
 document.getElementById("check").addEventListener("click", function() { 
     const Entry = document.getElementById("Entry").value;
+
     if(Entry.trim().toLowerCase()===word){
         alert("That is Correct, Well done")
+        TallyScore();
     }
     else {
-        alert("Sorry, That is not Correct")
+        alert("Sorry, Try again")
+        TallyMistake();
+    
     }
     document.getElementById("Entry").value='';
 });
 
+// function checkAnswer() {
+//     let userEntry = parseInt(document.getElementById("Entry").value);
+//     let CorrectEntry = check();
+//     let IsCorrect = userEntry === CorrectEntry[0];
+// }
 
-// This for the repeat button, provided for user to repeat the word in case they missed it the first time, copied and pasted another one to repeat again. 
-
-document.getElementById("generate").addEventListener("click", function(){
-    const utterance = new SpeechSynthesisUtterance();
-    utterance.text=word
-    speechSynthesis.speak(utterance)
-
-    document.getElementById("generate").addEventListener("click", function(){
-        const utterance = new SpeechSynthesisUtterance();
-        utterance.text=word
-        speechSynthesis.speak(utterance)
-    });
-});
+function TallyScore() {
+    let prevScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++prevScore;
+}
+function TallyMistake() {
+    let prevScore = parseInt(document.getElementById("mistake").innerText);
+    document.getElementById("mistake").innerText = ++prevScore;
+}
