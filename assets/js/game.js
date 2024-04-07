a7179a01a21e63780a67b3116726911f2cb99b1b
Frees1990 <fairmejor@gmail.com>
2024-04-07 21:49:26 +0000

    added alert to let user know it's game over/added index file to get users info/also added code to set users number of generated words

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,11 @@
+const playerName = getQueryParam("name","guest");
+let wordsCount = parseInt(getQueryParam("wordsCount", "10"));
+
 let word;
     // words to use for the spelling
 const words = ["accident", "accurate", "actual", "accept","accurate", "adult", "address", "afraid", "answer", "appear", "April", "arrive", "attention", "autumn", "believe", "bicycle", "birthday", "breath", "build", "busy", "business", "calculate", "calendar", "cartoon", "caught", "category", "catalog", "centre","century", "certain", "chocolate", "circle","complete", "communicacte", "consider","continue", "corpse", "computer", "credit", "couch", "current", "decide", "debt", "debit","delicious", "describe","different","difficult","disappear", "dessert", "dream", "early","earth","eight","enough", "exercise", "excited", "exception", "experience","experiment","extreme","famous","favourite", "figure", "forward","Friday","fruit", "general", "generate", "grammar","group","guard","guide", "heard","heart","height","history", "horrizon", "hour","imagine", "image", "increase", "important", "instinct", "interest", "island", "judge", "knowledge", "learn", "length", "library", "light", "magic", "material", "medicine", "mention", "minute", "mobile", "movie", "multiply", "natural", "narrate", "naughty", "notice", "occasion", "often", "opposite", "omnivor", "ordinary", "particular", "peculiar", "perhaps", "popular", "position", "possession", "possible", "potatoes", "pressure", "probably", "promise", "purpose", "quarter", "question", "radio", "random", "recent", "regular", "reign", "remember", "respect", "scent", "scene", "science", "seconds", "sense", "sentence", "separate", "simple","special", "shallow", "straight", "strange", "strength", "suppose", "surprise", "square", "tangle", "telephone", "television", "terrified", "therefore","though", "thought", "through", "triangle", "unite", "various", "weight", "woman", "xylophone"]
 
+let chances = 3;
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -13,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function() {
             } 
             else {
                 let spellingGame = this.getAttribute("id");
-                generate("id");
             }
         });
     }
@@ -28,6 +31,11 @@ document.getElementById("generate").addEventListener("click", function(){
         const utterance = new SpeechSynthesisUtterance();
         utterance.text=word
         speechSynthesis.speak(utterance)
+        wordsCount = wordsCount - 1;
+        if (wordsCount === 0) { 
+            document.getElementById("generate").disabled = true;
+            alert ("GAME OVER!!!" + playerName);
+        }
 });
 
 // This for the repeat button, provided for user to repeat the word in case they missed it the first time, copied and pasted another one to repeat again. 
@@ -49,21 +57,28 @@ document.getElementById("check").addEventListener("click", function() {
         TallyScore();
     }
     else {
-        alert("Sorry, Try again")
         TallyMistake();
-    
+        alert("Sorry, Try again")
+        if (chances === 0) {
+            alert("This is how you spell ---" + word) 
+        }
     }
     document.getElementById("Entry").value='';
 });
-
 
 // This is where the scores are incremented
 function TallyScore() {
     let prevScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++prevScore;
+    chances = 3;
 }
 function TallyMistake() {
     let prevScore = parseInt(document.getElementById("mistake").innerText);
     document.getElementById("mistake").innerText = ++prevScore;
+    chances = chances - 1;
 }
 
+function getQueryParam (param, defaultValue) { 
+    const urlParams = new URLSearchParams (window.location.search);
+    return urlParams.has(param) ? urlParams.get(param):defaultValue;
+}
