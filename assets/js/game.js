a765d8820cfbbc5b18b42e0b1630db9bf228440a
Frees1990 <fairmejor@gmail.com>
2024-04-17 13:41:41 +0000

    trying to figure out to make the generate word button generate word initially without clicking it

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,10 @@
-$("#Entry").focus();
-
+// index page to pass on plaer name and number of words
 const playerName = getQueryParam("name","guest");
 let wordsCount = parseInt(getQueryParam("wordsCount", "10"));
 
 let word;
     // words to use for the spelling
-const words = ["accident", "accurate", "actual", "accept","accurate", "adult", "address", "afraid", "answer", "appear", "April", "arrive", "attention", "autumn", "believe", "bicycle", "birthday", "breath", "build", "busy", "business", "calculate", "calendar", "cartoon", "caught", "category", "catalog", "centre","century", "certain", "chocolate", "circle","complete", "communicacte", "consider","continue", "corpse", "computer", "credit", "couch", "current", "decide", "debt", "debit","delicious", "describe","different","difficult","disappear", "dessert", "dream", "early","earth","eight","enough", "exercise", "excited", "exception", "experience","experiment","extreme","famous","favourite", "figure", "forward","Friday","fruit", "general", "generate", "grammar","group","guard","guide", "heard","heart","height","history", "horrizon", "hour","imagine", "image", "increase", "important", "instinct", "interest", "island", "judge", "knowledge", "learn", "length", "library", "light", "magic", "material", "medicine", "mention", "minute", "mobile", "movie", "multiply", "natural", "narrate", "naughty", "notice", "occasion", "often", "opposite", "omnivor", "ordinary", "particular", "peculiar", "perhaps", "popular", "position", "possession", "possible", "potatoes", "pressure", "probably", "promise", "purpose", "quarter", "question", "radio", "random", "recent", "regular", "reign", "remember", "respect", "scent", "scene", "science", "seconds", "sense", "sentence", "separate", "simple","special", "shallow", "straight", "strange", "strength", "suppose", "surprise", "square", "tangle", "telephone", "television", "terrified", "therefore","though", "thought", "through", "triangle", "unite", "various", "weight", "woman", "xylophone"]
+const words = ["accident", "accurate", "actual", "accept","accurate", "adult", "address", "afraid", "answer", "appear", "arrive", "attention", "autumn", "believe", "bicycle", "birthday", "breath", "build", "busy", "business", "calculate", "calendar", "cartoon", "caught", "category", "catalog", "centre","century", "certain", "chocolate", "circle","complete", "communicacte", "consider","continue", "corpse", "computer", "credit", "couch", "current", "decide", "debt", "debit","delicious", "describe","different","difficult","disappear", "dessert", "dream", "early","earth","eight","enough", "exercise", "excited", "exception", "experience","experiment","extreme","famous","favourite", "figure", "forward","Friday","fruit", "general", "generate", "grammar","group","guard","guide", "heard","heart","height","history", "horrizon", "hour","imagine", "image", "increase", "important", "instinct", "interest", "island", "judge", "knowledge", "learn", "length", "library", "light", "magic", "material", "medicine", "mention", "minute", "mobile", "movie", "multiply", "natural", "narrate", "naughty", "notice", "occasion", "often", "opposite", "omnivor", "ordinary", "particular", "peculiar", "perhaps", "popular", "position", "possession", "possible", "potatoes", "pressure", "probably", "promise", "purpose", "quarter", "question", "radio", "random", "recent", "regular", "reign", "remember", "respect", "scent", "scene", "science", "seconds", "sense", "sentence", "separate", "simple","special", "shallow", "straight", "strange", "strength", "suppose", "surprise", "square", "tangle", "telephone", "television", "terrified", "therefore","though", "thought", "through", "triangle", "unite", "various", "weight", "woman", "xylophone"]
 
 let chances = 3;
 
@@ -22,28 +21,23 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+    $(document).ready(function(){
+        $("#Entry").focus();
+        $("#check").keypress(function(){
+        });
+      });
 });
-
-$(document).ready(function(){
-    $("#check").keypress(function(){
-    });
-  });
-
 // This here is to generate the words above on shuffle"random" and alerts player that the game is over when he reaches up to the number of words the player entered in the index page
 
 document.getElementById("generate").addEventListener("click", function(){
     currentWord = Math.floor(Math.random() * words.length);
     word=words[currentWord]
-
         const utterance = new SpeechSynthesisUtterance();
         utterance.text=word
         speechSynthesis.speak(utterance)
         wordsCount = wordsCount  - 1;
         $("#Entry").focus();
-        
 });
-
-
 // This for the repeat button, provided for user to repeat the word in case they missed it the first time, copied and pasted another one to repeat again. 
 
 document.getElementById("talk").addEventListener("click", function(){
@@ -51,7 +45,6 @@ document.getElementById("talk").addEventListener("click", function(){
     utterance.text=word
     speechSynthesis.speak(utterance)
 });
-
 
 // This here is for the check button to check if the word entered was correct or not, there is alsoo an alert to let the user know if the word enetered is correct. 
 
@@ -103,7 +96,7 @@ function TallyMistake() {
     document.getElementById("mistake").innerText = ++prevScore;
     chances = chances - 1;
 }
-
+// index page to pass on plaer name and number of words
 function getQueryParam (param, defaultValue) { 
     const urlParams = new URLSearchParams (window.location.search);
     return urlParams.has(param) ? urlParams.get(param):defaultValue;
